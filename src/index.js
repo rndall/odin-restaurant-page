6a20e55bf8547cb93756fcd1983237cfb3522bc3
rndall <96964503+rndall@users.noreply.github.com>
2025-01-25 23:14:35 +0800

    Add tab switching

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import "./style.css";
+import loadAbout from "./pages/About";
 import loadHome from "./pages/Home";
+import loadMenu from "./pages/Menu";
+
+const tabButtons = document.querySelectorAll(".nav__button");
+const contentDiv = document.querySelector("#content");
+
+function handleSwitchTab(e) {
+	const tab = e.target.dataset.tab;
+
+	contentDiv.textContent = "";
+
+	switch (tab) {
+		case "home":
+			loadHome();
+			break;
+
+		case "menu":
+			loadMenu();
+			break;
+
+		case "about":
+			loadAbout();
+			break;
+	}
+}
+
+for (const btn of tabButtons) {
+	btn.addEventListener("click", handleSwitchTab);
+}
 
 loadHome();
