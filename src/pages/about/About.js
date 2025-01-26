0b87b8e44fb08328a623d4ecc92ff8bff39c32d5
rndall <96964503+rndall@users.noreply.github.com>
2025-01-26 16:20:16 +0800

    Add about page styles

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -1,3 +1,5 @@
+import "./about.css";
+
 function createAbout() {
 	const aboutPage = document.createElement("div");
 
@@ -7,6 +9,7 @@ function createAbout() {
 		"about__title",
 		"about__title--color-white",
 		"about__title--6xl",
+		"about__title--banner",
 	);
 
 	const headingSection = document.createElement("div");
@@ -19,21 +22,29 @@ function createAbout() {
 		"about__title",
 		"about__title--color-orange",
 		"about__title--weight-thin",
+		"about__title--4xl",
 	);
 
 	const p1 = document.createElement("p");
 	p1.textContent =
 		"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit amet ipsam sit reiciendis corrupti totam itaque at. Labore similique quis magnam error at consectetur rerum? Excepturi aut facilis numquam quas.";
+	p1.classList.add("about__text");
 
 	const p2 = document.createElement("p");
 	p2.textContent =
 		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore alias, laboriosam a provident dolore dolorum facere in unde quaerat corporis nam modi nisi sapiente atque mollitia tempore tempora nemo ea.";
+	p2.classList.add("about__text");
 
 	const p3 = document.createElement("p");
 	p3.textContent =
 		"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur quis optio accusamus voluptates deserunt laboriosam nobis, praesentium ratione eum inventore!";
+	p3.classList.add("about__text");
 
-	aboutPage.append(headingSection, h2, p1, p2, p3);
+	const textDiv = document.createElement("div");
+	textDiv.classList.add("about__text-content");
+	textDiv.append(h2, p1, p2, p3);
+
+	aboutPage.append(headingSection, textDiv);
 
 	return aboutPage;
 }
